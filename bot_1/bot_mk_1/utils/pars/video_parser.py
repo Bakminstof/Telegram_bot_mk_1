de259d39a3d2cc774f6728a7b0775f9b678f686b
andrey <bakminstof@yandex.ru>
2022-07-11 13:29:30 +0300

    Fix refactor proc

--- bot_1/bot_mk_1/utils/pars/video_parser.py
+++ bot_1/bot_mk_1/utils/pars/video_parser.py
@@ -6,7 +6,6 @@ import logging.config
 import time
 import urllib3
 
-
 from typing import Dict, List, Any
 from aiogram import Dispatcher
 from threading import Thread
@@ -36,7 +35,6 @@ class VideoParser:
         self.timeout = 10
         self.channels = {}
         self.updates = {}
-        self.barrier = multiprocessing.Barrier(1)
 
     # get channels data
     @logger_decorator(channel_logger)
@@ -46,17 +44,17 @@ class VideoParser:
 
         for channel in channel_data:
             self.channels[channel[0]] = {
-                    'channel_id': channel[0],
-                    'location': channel[1],
-                    'last_video': channel[2],
-                    'custom_name': channel[3],
-                    'user_id': channel[4],
-                }
+                'channel_id': channel[0],
+                'location': channel[1],
+                'last_video': channel[2],
+                'custom_name': channel[3],
+                'user_id': channel[4],
+            }
 
         channel_logger.debug(f'Channels data: {self.channels}')
 
     # get channel's HTML page. Thread work
-    def __get_html_page(self) -> None:
+    def __get_html_page(self, barrier: multiprocessing.Barrier) -> None:
         channel_logger.debug('Load HTML pages')
 
         threads = [
@@ -64,7 +62,8 @@ class VideoParser:
                 url=channel.get('location') + '/videos',
                 timeout=self.timeout,
                 id_channel=id_,
-                channels=self.channels
+                channels=self.channels,
+                barrier=barrier
             ) for id_, channel in self.channels.items()
         ]
 
@@ -89,7 +88,7 @@ class VideoParser:
         parts_keys = []
 
         manager = Manager()
-        dict_manager = self.updates = manager.dict()
+        updates_manager = manager.dict()
 
         if len(channels_keys) <= 30:
             parts_keys.append(channels_keys)
@@ -105,8 +104,8 @@ class VideoParser:
                     ChanelRefactorProcess(
                         id_channel=id_channel,
                         channels=self.channels,
-                        updates=self.updates,
-                        args=(dict_manager,)
+                        updates=updates_manager,
+                        args=(updates_manager,)
                     )
                 ]
                 workers = []
@@ -120,6 +119,9 @@ class VideoParser:
 
                     del worker
 
+        self.updates.update(updates_manager)
+
+        del updates_manager
         channel_logger.debug('End refactoring')
 
     # searching new videos
@@ -129,13 +131,13 @@ class VideoParser:
             channel_logger.debug('Start checking')
 
             await self.__get_channels_data()
+            barrier = multiprocessing.Barrier(len(self.channels.keys()))
 
             if self.channels:
-                self.__get_html_page()
+                self.__get_html_page(barrier=barrier)
                 self.__refactor_channels()
 
                 if self.updates:
-
                     await self.__update_db()
                     await self.__send_new_videos()
 
@@ -143,7 +145,7 @@ class VideoParser:
                 self.channels.clear()
                 self.updates.clear()
 
-            time_ = random.randint(3000, 5000)  # sec
+            time_ = random.randint(5000, 7000)  # sec
             channel_logger.info(f'Go sleep {time_} sec.')
             await asyncio.sleep(time_)  # sleep
 
@@ -162,7 +164,14 @@ class VideoParser:
         await a_session_main.run_sync(lambda session: session.bulk_update_mappings(SubscriptionsTable, to_update))
         await a_session_main.commit()
 
-        channel_logger.debug('Done update')
+        channel_logger.debug(
+            'Done update: {}'.format(
+                [
+                    c.get('custom_name')
+                    for c in self.updates.values()
+                ]
+            )
+        )
 
     # send new videos to users
     @logger_decorator(channel_logger)
@@ -192,7 +201,7 @@ class VideoParser:
                 )
             )
             for channel in self.updates.values()
-            if notif.get(channel.get('user_id'))
+            if notif.get(channel.get('user_id')) and channel.get('new_videos', False)
         ]
 
         await asyncio.gather(*to_send)
@@ -205,23 +214,23 @@ class HTMLThread(Thread):
             self,
             url: str,
             timeout: int,
-            id_channel: int = None,
-            channels: Dict[int, Dict[str, Any]] = None,
-            for_result: list | None = None
+            barrier: multiprocessing.Barrier,
+            id_channel: int,
+            channels: Dict[int, Dict[str, Any]]
     ):
         super().__init__()
         self.timeout = timeout
         self.url = url
         self.id_ = id_channel if id_channel else self.native_id
-        self.logger_base_msg = '{name} id=\'{id_}\' Try №{try_}'
+        self.logger_base_msg = '{name} id=\'{id_}\' Try №{try_}, url: {url}'
         self.channels = channels
-        self.result = for_result
         self.http_status_code = None
+        self.barrier = barrier
 
     def run(self) -> None:
-        for try_ in range(1, 4):
+        for try_ in range(1, 5):
             try:
-                channel_logger.debug(self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_))
+                channel_logger.debug(self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_, url=self.url))
 
                 http = urllib3.PoolManager()
                 resp = http.request('GET', url=self.url, timeout=self.timeout)
@@ -234,24 +243,27 @@ class HTMLThread(Thread):
                         self.logger_base_msg.format(
                             name=self.name,
                             id_=self.id_,
-                            try_=try_)
+                            try_=try_,
+                            url=self.url
+                        )
                     )
                 )
 
-                if self.channels:
-                    self.channels[self.id_]['html_page'] = text
-
-                if isinstance(self.result, list):
-                    self.result.append(text)
+                self.channels[self.id_]['html_page'] = text
 
                 http.clear()
+                del http
+
+                self.barrier.wait()
 
                 break
 
             except Exception as ex:
+                del http
+
                 random_time = random.randint(5, 15)
                 channel_logger.warning(
-                    f'{self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_)} => exception:'
+                    f'{self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_, url=self.url)} => exception:'
                     f'\n\ttype -> "{type(ex)}",'
                     f'\n\ttext -> ({ex})'
                     f'\n\tgo sleep {random_time} sec. and try again')
@@ -260,12 +272,14 @@ class HTMLThread(Thread):
                 continue
         else:
             try_ = 'last try'
-
-            channel_logger.warning(
-                f'{self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_)} => critical'
+            channel_logger.error(
+                f'{self.logger_base_msg.format(name=self.name, id_=self.id_, try_=try_, url=self.url)} => error'
                 f'\nCant get html page:'
                 f'\n\turl="{self.url}"'
             )
+
+            self.channels[self.id_]['html_page'] = None
+            self.barrier.wait()
 
 
 class ChanelRefactorProcess(Process):
@@ -278,20 +292,25 @@ class ChanelRefactorProcess(Process):
     ):
         super().__init__(args=args)
         # global
-        self.channels = channels
-        self.updates = updates
+        self.updates: dict = updates
         # current
-        self.id_channel = id_channel
-        self.url: str = self.channels.get(self.id_channel).get('location')
-        self.html_page: str = self.channels.get(self.id_channel).get('html_page')
-        self.last_video: str = self.channels.get(self.id_channel).get('last_video')
-        self.custom_name: str = self.channels.get(self.id_channel).get('custom_name')
+        self.id_channel: int = id_channel
+        self.current_channel: dict = channels.get(id_channel)
+        self.url: str = channels.get(self.id_channel).get('location')
+        self.html_page: str = channels.get(self.id_channel).get('html_page')
+        self.last_video: str = channels.get(self.id_channel).get('last_video')
+        self.custom_name: str = channels.get(self.id_channel).get('custom_name')
         self.all_videos = []
 
     def run(self) -> None:
         channel_logger.debug(f'Start proc id: {self.pid}')
-        self.all_videos = self.__scrap_videos(html_page=self.html_page, url=self.url)
-        self.__comparison_func()
+        html = self.html_page
+
+        if html:
+            self.all_videos = self.__scrap_videos(html_page=html, url=self.url)
+            self.__comparison_func()
+        else:
+            channel_logger.error(f'Proc \'{self.pid}\', url {self.url} HTML page is None')
 
     @staticmethod
     def __scrap_videos(html_page: str, url: str) -> List[str | None]:
@@ -310,7 +329,8 @@ class ChanelRefactorProcess(Process):
 
         for video in self.all_videos:
             if self.last_video not in self.all_videos:
-                self.channels[self.id_channel]['last_video'] = self.all_videos[0]
+                self.current_channel['last_video'] = self.all_videos[0]
+                self.updates[self.id_channel] = self.current_channel
                 break
 
             elif video != self.last_video:
@@ -327,8 +347,7 @@ class ChanelRefactorProcess(Process):
                     v=', '.join(new_videos)
                 )
             )
-            self.channels[self.id_channel]['new_videos'] = new_videos
-            self.channels[self.id_channel]['last_video'] = new_videos[0]
 
-            channel = self.channels.get(self.id_channel)
-            self.updates[self.id_channel] = channel
+            self.current_channel['new_videos'] = new_videos
+            self.current_channel['last_video'] = new_videos[0]
+            self.updates[self.id_channel] = self.current_channel
